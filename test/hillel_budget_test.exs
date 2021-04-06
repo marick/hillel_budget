47b9052512ef8bc52a52ffabf8203743a02fdc60
Brian Marick <marick@exampler.com>
2021-04-06 17:53:57 -0500

    Total budget boundaries

--- test/hillel_budget_test.exs
+++ test/hillel_budget_test.exs
@@ -1,14 +1,21 @@
 defmodule HillelBudgetTest do
   use ExUnit.Case
 
-  def can_afford?(_budget, _bill) do
-    true
+  def can_afford?(budget, bill) do
+    total_charge =
+      (for item <- bill, do: item.cost)
+      |> Enum.sum
+    total_charge <= budget.total_limit
   end
 
-  test "total budget always passes" do
-    budget = %{total_limit: 50}
-    bill = %{cost: 50}
+  def budget(total_limit) do
+    %{total_limit: total_limit}
+  end
 
-    assert can_afford?(budget, bill)
+  test "total budget boundaries" do
+    bill = [%{cost: 50}]
+
+    assert can_afford?(budget(50), bill)
+    refute can_afford?(budget(49), bill)
   end
 end
