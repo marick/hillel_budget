9d68ba5d283a81529bc1687d9c0ee95e5e777de0
Brian Marick <marick@exampler.com>
2021-04-06 17:45:32 -0500

    New project

---
+++ lib/hillel_budget.ex
@@ -0,0 +1,18 @@
+defmodule HillelBudget do
+  @moduledoc """
+  Documentation for `HillelBudget`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HillelBudget.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hillel_budget_test.exs
@@ -0,0 +1,8 @@
+defmodule HillelBudgetTest do
+  use ExUnit.Case
+  doctest HillelBudget
+
+  test "greets the world" do
+    assert HillelBudget.hello() == :world
+  end
+end
