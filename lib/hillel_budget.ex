671c3246e0a195919868eba8625fdf8ac2f2795d
Brian Marick <marick@exampler.com>
2021-04-08 14:31:01 -0500

    Don’t check categories if budget is already blown.

--- lib/hillel_budget.ex
+++ lib/hillel_budget.ex
@@ -4,24 +4,22 @@ defmodule HillelBudget do
   def can_afford?(budget, items) do
     items = Item.normalize(items)
 
-    total_ok = the_total_budget_is_ok(budget.total_limit, items)
-    categories_ok = all_category_budgets_are_ok(budget.category_limits, items)
-
-    total_ok && categories_ok
+    if the_total_budget_is_ok?(budget.total_limit, items), 
+    do: all_category_budgets_are_ok?(budget.category_limits, items),
+    else: false
   end
   
   # ----------------------------------------------------------------------------
   
-  def the_total_budget_is_ok(limit, items) do
-    item_charges = for item <- items do
-      count = Map.get(item, :count, 1)
-      item.cost * count
-    end
-      
-    limit - Enum.sum(item_charges) >= 0
+  def the_total_budget_is_ok?(limit, items) do
+    item_charges = for item <- items, do: item.cost
+    limit >= Enum.sum(item_charges)
   end
 
-  def all_category_budgets_are_ok(category_limits, items) do
-    not Enum.empty?(LimitHolder.surviving_holders([category_limits], items))
+  def all_category_budgets_are_ok?(category_limits, items) do
+    case LimitHolder.surviving_holders([category_limits], items) do
+      [] -> false
+      _ -> true
+    end
   end
 end
