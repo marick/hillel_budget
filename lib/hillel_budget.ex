182fd8b7cd2249e55ef12da668f3d4b13bc93bb2
Brian Marick <marick@exampler.com>
2021-04-08 12:14:47 -0500

    sketch final version of can_afford?

--- lib/hillel_budget.ex
+++ lib/hillel_budget.ex
@@ -1,17 +1,21 @@
 defmodule HillelBudget do
   alias HillelBudget.Item
   
-  def remaining_total_after_bill(limit, bill) do
+  def the_total_budget_is_ok(limit, bill) do
     item_charges = for item <- bill do
       count = Map.get(item, :count, 1)
       item.cost * count
     end
       
-    limit - Enum.sum(item_charges)
+    limit - Enum.sum(item_charges) >= 0
   end
 
   def can_afford?(budget, bill) do
     items = Item.normalize(bill)
-    remaining_total_after_bill(budget.total_limit, items) >= 0
+
+    total_ok = the_total_budget_is_ok(budget.total_limit, items)
+    categories_ok = true
+
+    total_ok && categories_ok
   end
 end
